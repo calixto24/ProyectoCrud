acdc09a295821b36d3ae349acdef7b2a1b984b17
Jhordan <calixto4211@gmail.com>
2024-09-04 14:00:01 -0500

    creando el formulario

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,16 @@
+
+package main;
+
+import vista.FormPrincipal;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        FormPrincipal formPrincipal = new FormPrincipal();
+        
+        formPrincipal.setVisible(true);
+        
+    }
+    
+}
